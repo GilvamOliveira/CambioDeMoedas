e5750cfd5e11ffb3ef1c919e0498e5d53962e447
GilvamOliveira <oliveiragilvam70@gmail.com>
2026-02-22 15:40:31 -0300

    updates

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import br.com.currencyapp.controller;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        new controller().start();
+
         }
     }
